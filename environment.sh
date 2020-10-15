869eb00ce08dc9fac6830d17f213d43756035643
Jerfeson Guerreiro <jerfeson@p21sistemas.com.br>
2020-10-15 10:45:53 -0300

    :tada: atualizando

--- environment.sh
+++ environment.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 echo "##### Iniciando ambiente #####"
 
+echo "##### Baixando arquivos essenciais #####"
+wget https://raw.githubusercontent.com/p21sistemas/docker-ap/master/exec.sh && chmod +x ambiente.sh
+wget https://raw.githubusercontent.com/p21sistemas/docker-ap/master/start.sh && chmod +x start.sh
+wget https://raw.githubusercontent.com/p21sistemas/docker-ap/master/sample.env
+
+echo "##### Por favor informa o nome do projeto com _ no final Ex. sdt21_df_ #####"
+read ENV_PROJECT
+
+echo "##### Por favor informa a porta em que o projeto vai estar externamente Ex. 8080 #####"
+read ENV_PORT
+
+echo "##### Por favor informa o ambiente do projeto Ex. desenvolvimento, teste ou producao #####"
+read ENV_APP
+
+sed -i "s/ENV_PROJECT/$ENV_PROJECT/g" sample.env
+sed -i "s/ENV_PORT/$ENV_PORT/g" sample.env
+sed -i "s/ENV_APP/$ENV_APP/g" sample.env
+
 echo "##### Copiando arquivos de configuração #####"
 cp .env bin/webserver/
 cp start.sh bin/webserver/
@@ -25,6 +43,8 @@ docker-compose -f docker-compose.yml up -d --force
 echo "##### Removendo arquivo de configuração #####"
 rm bin/webserver/.env
 rm bin/webserver/start.sh
+rm environment.sh exec.sh start.sh sample.env
+
 
 echo '##### Acessando o ambiente e executando primeiros comandos #####'
 
