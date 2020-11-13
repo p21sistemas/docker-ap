66f6f390462b6c8ccb695fd12af69b70ebbf8630
Jerfeson Guerreiro <jerfeson_guerreiro@hotmail.com>
2020-11-13 18:30:53 -0300

    :sparkles: atualizado skeleton de instalação.

--- environment.sh
+++ environment.sh
@@ -65,7 +65,7 @@ if [ ! -f .env ]; then
 
   if [ ! -d "www/" ]; then
     echo "##### Configuração inicial do projeto #####"
-    mkdir --parents www/; mv !(teste.sh) $_
+    mkdir --parents www/; mv !(init.sh) $_
   fi
 
   if [ -d $DIRECTORY_NGINX ]; then
