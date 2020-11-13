db6f1f75a3708b1d79634d99f1a0ddbd0265cbfa
Jerfeson Guerreiro <jerfeson_guerreiro@hotmail.com>
2020-11-13 18:39:24 -0300

    :sparkles: atualizado skeleton de instalação.

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm environment.sh
+rm -f environment.sh
 wget --no-check-certificate --no-cache --no-cookies https://raw.githubusercontent.com/p21sistemas/docker-ap/master/environment.sh && chmod +x environment.sh && ./environment.sh
