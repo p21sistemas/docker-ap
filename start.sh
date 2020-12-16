6806963b9de68af43f32d32a89cda24e3df816f5
Jerfeson Guerreiro <jerfeson@p21sistemas.com.br>
2020-12-16 16:39:33 -0300

    :sparkles: php version

--- start.sh
+++ start.sh
@@ -9,6 +9,8 @@ read_var() {
 }
 
 APPLICATION_ENV=$(read_var APP_ENV .env)
+PHP_ENV=$(read_var PHP_VERSION .env)
+
 
 # Cria um arquivo de configuração para o apache, de forma global
 touch /etc/apache2/conf-enabled/environment.conf
@@ -18,19 +20,28 @@ echo SetEnv APP_ENV "'$APPLICATION_ENV'">> /etc/apache2/conf-enabled/environment
 # Coisas do ambiente de desenvolvimento
 if [ $APPLICATION_ENV == 'desenvolvimento' ]; then
 
-    #Install XDebug
-    pecl install xdebug-2.5.5
+
+    if [ PHP_ENV == '72' ]; then
+       pecl install xdebug
+       XDEBUG= 'zend_extension=//usr/lib/php/20170718/xdebug.so'
+       PHP='7.2'
+    elif [ PHP_ENV == '56' ]; then
+      #Install XDebug
+       pecl install xdebug-2.5.5
+       XDEBUG= 'zend_extension=/usr/lib/php/20131226/xdebug.so'
+       PHP='5.6'
+    fi
 
     #Configuration XDebug
-    echo 'zend_extension=/usr/lib/php/20131226/xdebug.so' >> /etc/php/5.6/apache2/php.ini
-    echo 'zend_extension=/usr/lib/php/20131226/xdebug.so' >> /etc/php/5.6/cli/php.ini
+    echo $XDEBUG >> /etc/php/$PHP/apache2/php.ini
+    echo $XDEBUG >> /etc/php/$PHP/cli/php.ini
 
     mkdir "/conf.d" && version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
         && curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/linux/amd64/$version \
         && mkdir -p /tmp/blackfire \
         && tar zxpf /tmp/blackfire-probe.tar.gz -C /tmp/blackfire \
         && mv /tmp/blackfire/blackfire-*.so $(php -r "echo ini_get ('extension_dir');")/blackfire.so \
-        && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > /etc/php/5.6/apache2/conf.d/blackfire.ini
+        && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > /etc/php/$PHP/apache2/conf.d/blackfire.ini
 
     #limpando lixo
     rm -rf /tmp/pear \
